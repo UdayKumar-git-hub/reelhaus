fc3b6bd99cfc34d0e1da7ff0587257a461ede3fa
UdayKumar-git-hub <udayl4905@gmail.com>
2025-09-01 15:40:45 +0530

    Updated supabaseEventClient.js

--- src/supabaseEventClient.js
+++ src/supabaseEventClient.js
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Keys from your NEW "ReelHaus Events" project
-const supabaseUrl = 'YOUR_NEW_SUPABASE_PROJECT_URL'; 
-const supabaseAnonKey = 'YOUR_NEW_SUPABASE_ANON_KEY';
+const supabaseUrl = 'https://iklwclewqvzblrgnbhfp.supabase.co'; 
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlrbHdjbGV3cXZ6YmxyZ25iaGZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY3MTc5ODMsImV4cCI6MjA3MjI5Mzk4M30.5u-FF8boLQTaCpPF3eMl2voL3TQhuAf94LFPqp8tr1M';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
